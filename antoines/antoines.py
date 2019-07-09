f716fb89a990a7901e92ee44475779d75e8dd5c2
EliteFourRhys <rhysvollmer@gmail.com>
2019-07-09 15:18:34 -0400

    updates antoines

--- antoines/antoines.py
+++ antoines/antoines.py
@@ -1,23 +1,35 @@
 import pandas as pd
 
 def main():
-    constants()
+    succes = False
+    # loop until you get succesful calculation for paramaters
+    lst = user_input()
+    data = constants(lst)
 
-def search_text():
-    print('one second')
+def user_input():
+    print('just need a few things')
+    print("An example to find VP at given temp: H20 Water 25 None")
+    inpt = input("Please enter data in the form: Formula Name GivenTemp GivenVP :")
+    lst = inpt.split()
+    return lst
 
-def get_setup():
-    print('just need some things....')
-
-def constants():
+def constants(usr):
     print('stuff')
-    name = 'some_example_name'
+
+    # provide a pre screen function that matches names to id to improve quary use
+    name = 'Water'
+    ident = 'some_example_id'
+
+    # load csv into a pandas df
     df = pd.read_csv('Antoine_Coefficients.csv')
-    print(df)
-    for rows in df.iterrows():
-        print(rows)
-        
-    print(df.query('ID == 2'))
+
+    # iterating rows to slow, try to avoid using these lines
+    # for rows in df.iterrows():
+
+    # this will take a user provided querey and return the constants, whats nice about this is it can return all rows for multiple temp ranges
+    print(df[df['Name'].str.match(str(name))])
+    data = df[df['Name'].str.match(str(name))]
+    return data
 
 def calculation():
     print('calculating....')
