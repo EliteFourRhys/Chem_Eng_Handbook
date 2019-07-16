a88624e01f38ff0be03d01bb971dd97e573b12d8
EliteFourRhys <rhysvollmer@gmail.com>
2019-07-16 00:46:59 -0400

    some changes

--- mol_mass_converter/MoleConvert.py
+++ mol_mass_converter/MoleConvert.py
@@ -1,34 +1,44 @@
 import xlrd
-loc="C:\Users\ASUS\Downloads\PYTHON and shiit\Data_Set.xlsx";
-st=input("Enter an element/compound");
-SampleMoles=input("Enter number of moles or n if you dont know");
-SampleMass=input("Enter the mass or m if you dont know");
-workbook=xlrd.open_workbook(loc);
-sheets=workbook.sheet_by_index(0);
-Find_Moles(SampleMoles,SampleMass)
-Find_Mass(SampleMoles,SampleMass)
+
+loc="Data_Set.xlsx"
+st=input("Enter an element/compound")
+
+def main():
+   global sheets
+   SampleMoles=input("Enter number of moles or n if you dont know")
+   SampleMass=input("Enter the mass or m if you dont know")
+   workbook=xlrd.open_workbook(loc)
+   sheets=workbook.sheet_by_index(0)
+   Find_Moles(SampleMoles,SampleMass)
+   Find_Mass(SampleMoles,SampleMass)
+
 def Find_Moles(Moles,Mass):
-		for i in sheet.nrows:
-			if(st.casefold()==sheets.cell_value(i,1).casefold()):
-				Molar_Mass=sheets.cell_value(i,3);
-				Calc_Moles=Mass/Molar_Mass;
-				output=print("The number of moles is "+Calc_Moles+" and the molar mass is "+Molar_Mass);
-				break
-	if(type(Moles) is int):	
-		output=print("The number of moles is " +Moles+ "and the molar mass is "+Molar_Mass);
-   return output
+    global sheets
+    print(sheets.nrows)
+    for i in range(sheets.nrows):
+        if (st.casefold()==sheets.cell_value(i,1).casefold()):
+            Molar_Mass=sheets.cell_value(i,3)
+            Calc_Moles=int(Mass)/Molar_Mass
+            output=print("The number of moles is "+str(Calc_Moles)+" and the molar mass is "+str(Molar_Mass))
+            break
+    if(type(Moles) is int):	
+        output=print("The number of moles is " +Moles+ "and the molar mass is "+Molar_Mass)
+    return output
 
 
 def Find_Mass(Moles,Mass):
-	if (Mass=="m"):
-		for i in sheet.nrows:
-			if(st.casefold()==sheets.cell_value(i,1).casefold()):
-				Molar_Mass=sheets.cell_value(i,3);
-				Calc_Mass=Moles*Molar_Mass;
-				output=print"The mass of the sample is "+Calc_Mass);
-				break
-	else:
-		output=print("The sample mass is  "+Mass);
-  return output
+    global sheets
+    if (Mass=="m"):
+      for i in sheets.nrows:
+            if (st.casefold()==sheets.cell_value(i,1).casefold()):
+                Molar_Mass=sheets.cell_value(i,3)
+                Calc_Mass=Moles*Molar_Mass
+                output=print("The mass of the sample is "+Calc_Mass)
+                break
+    else:
+	    output=print("The sample mass is  "+Mass)
+    return output
 
+if __name__ == "__main__":
+	main()
 
